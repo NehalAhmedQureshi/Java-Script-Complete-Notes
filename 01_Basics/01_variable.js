66d3e55268be2abeec4bd5ee0231c16f7bffc81e
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-14 13:15:20 +0500

    variable complete structure

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,5 +1,11 @@
 const accountId = 990123 ;
 let accountEmail = "example@domain.com" ;
-var accountPassword = "1234" ;
+var accountPassword = "1234" ; // this key word is not use yet
 region = "Pakistan" ;
 
+// accountId = another value // is not possible
+
+console.log(accountId);
+
+
+console.table([accountId , accountEmail , accountPassword , region]);
