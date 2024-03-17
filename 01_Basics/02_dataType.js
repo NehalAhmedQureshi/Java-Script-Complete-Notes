1fd1697403f4fbdc05945a1549c214709c8521ef
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-17 23:39:14 +0500

    add operations topic

--- 01_Basics/02_dataType.js
+++ 01_Basics/02_dataType.js
@@ -11,12 +11,50 @@ let symbol = Symbol("©")
 let Sample = null;
 
 
-console.log(typeof Name) //string
-console.log(typeof number) // number
-console.log(typeof follow) // boolean
-console.log(typeof sum) // bigInt
-console.log(typeof sample) // undefined
-console.log(typeof symbol) // special symbol
-console.log(typeof Sample) // object or null 
+// console.log(typeof Name) //string
+// console.log(typeof number) // number
+// console.log(typeof follow) // boolean
+// console.log(typeof sum) // bigInt
+// console.log(typeof sample) // undefined
+// console.log(typeof symbol) // special symbol
+// console.log(typeof Sample) // object or null 
 
-console.table([typeof Name ,typeof  number ,typeof  follow ,typeof  sum ,typeof  sample ,typeof  sample , typeof Sample])
+// console.table([typeof Name ,typeof  number ,typeof  follow ,typeof  sum ,typeof  sample ,typeof  sample , typeof Sample])
+
+// ****************** OPERATION ******************
+
+let value = 3 ;
+let negValue = -value ;
+
+// console.log(negValue);
+
+// console.log( 2 + 2 );
+// console.log( 2 - 2 );
+// console.log( 2 * 2 );
+// console.log( 2 ** 2 );
+// console.log( 2 % 2 );
+// console.log( 2 / 2 );
+
+let str1 = "hello" ;
+let str2 = " Nehal" ;
+
+// console.log( str1 + str2 ) ;
+
+// console.log( 2 + "3") ;
+// console.log( "2" + 3 ) ;
+// console.log( "2" + "3" ) ;
+// console.log( "2" + 3 + 1 ) ;
+// console.log( 2 + 3 + "1" ) ;
+
+// console.log( (2 + 2 ) * 3 % 4) ;
+
+// console.log( true );
+// console.log( +true );
+// console.log( +"" );
+
+let gameCounter = 100 ; 
+gameCounter++ ;
+
+// console.log( gameCounter );
+
+
