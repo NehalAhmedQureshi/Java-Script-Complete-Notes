e054cb4d628db3b3108616026b25da2bda24a2f2
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-19 11:57:49 +0500

    add new topic of comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null < 0);
+// console.log(null == 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+
+// === is strictly check operator which check the data type is same or not !
+
+console.log("2" == 2);  //true
+console.log("2" === 2);  // false
