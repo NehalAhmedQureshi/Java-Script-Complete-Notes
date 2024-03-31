a6641fd1a1211f190ec2f5680eabdc7feab36afb
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-01 00:54:12 +0500

    string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,13 @@
+const name = "Nehal"
+const repoCount = 20
+
+// ----------------------------- OLD WAY TO CONGATINATE STRING ------------------------
+// console.log(name + repoCount + " value ");
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ MODERN WAY TO CONGATENATE STRING ^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+console.log(`Hello ${name} your repo count is ${repoCount}`);
+
+const guestName = new String('Nehal');
+
+console.log(guestName);
